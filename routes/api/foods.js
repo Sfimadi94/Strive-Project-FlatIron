509e6a44f50d82cac4bebe11294d97f3ab7f71f8
Sfimadi94 <sydeffect1994@gmail.com>
2019-10-04 09:25:23 -0500

    updated

--- routes/api/foods.js
+++ routes/api/foods.js
@@ -1,38 +1,40 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const Food = require('../../models/Food')
+const Food = require('../../models/Food');
 
 router.get('/', (req, res) => {
-    Food.find()
+  Food.find()
     .then(foods => {
-        res.json(foods);
-      }).catch(err => { console.log(err)})
-})
+      res.json(foods);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-router.get('/:id', (req, res)=> {
-    Food.findById(req.params.id)
+router.get('/:id', (req, res) => {
+  Food.findById(req.params.id)
     .then(food => res.json(food))
-    .catch(err => res.status(404).json("Error: " + err))
-})
+    .catch(err => res.status(404).json('Error: ' + err));
+});
 
 router.post('/add', (req, res) => {
-    const newFood = new Food({
-        name: req.body.name,
-        calories: Number(req.body.calories)
-    })
+  const newFood = new Food({
+    name: req.body.name,
+    calories: Number(req.body.calories)
+  });
 
-    newFood.save()
-    .then((item)=> res.json(item))
-    .catch(err => res.status(400).json('Error: ', err))
-})
+  newFood
+    .save()
+    .then(item => res.json(item))
+    .catch(err => res.status(400).json('Error: ', err));
+});
 
 router.delete('/:id', (req, res) => {
-    Food.findById(req.params.id)
-    .then(food => food.remove().then(() => res.json({success: true})))
-    .catch(err => res.status(404).json({success: false}))
+  Food.findById(req.params.id)
+    .then(food => food.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
+});
 
-})
-
-
-module.exports = router
+module.exports = router;
